ec69d3d6cacd41b537d31a970c35956a2cac2c6b
kruthik <kruthik22bs@gmail.com>
2023-10-20 09:40:32 +0530

    updated employee details form to have reporting to input

--- client/scripts/employee-details.js
+++ client/scripts/employee-details.js
@@ -73,10 +73,14 @@ phoneNumber.disabled = true;
 const inputRole = document.querySelector('#inputRole');
 inputRole.disabled = true;
 
+const inputReporting = document.querySelector('#inputReporting');
+inputReporting.disabled = true;
+
 const editButton = document.querySelector('#edit-button');
 editButton.addEventListener('click', () => {
   inputName.disabled = false;
   inputDepartment.disabled = false;
   phoneNumber.disabled = false;
   inputRole.disabled = false;
+  inputReporting.disabled = false;
 });
