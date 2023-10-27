ed548fac83191f3d728c56b0d90edc56c8b8823f
Santhosh-BC <santhoshbc555@gmail.com>
2023-10-27 09:54:27 +0530

    Fixed some minor bugs related to leaves module

--- src/leave/leave.controller.ts
+++ src/leave/leave.controller.ts
@@ -1,16 +1,16 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { LeavesService } from './leave.service';
 
-@Controller('leaves')
+@Controller('api/leaves')
 export class LeavesController {
 
         constructor(private service: LeavesService) {}
 
-        @Get('viewAllEmployees')
-        @Render('viewEmployees')
-        async getAllEmployeeDetails() {
-          const employees = await this.service.getAllLeaves();
-          return { employees: employees };
+        @Get('viewAllLeaves')
+        @Render('leaveRequests')
+        async getAllLeaveDetails() {
+          const leaves = await this.service.getAllLeaves();
+          return { leaves: leaves };
         }
       }
       
