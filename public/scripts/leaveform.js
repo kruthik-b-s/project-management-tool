736cc61c96f574304528ad678de595ce6f2655af
kruthik <kruthik22bs@gmail.com>
2023-11-03 10:31:49 +0530

    updated the styles for leave form views

--- public/scripts/leaveform.js
+++ public/scripts/leaveform.js
@@ -1,33 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function optionSelected() {
+  const leaveType = document.querySelector('#leave_type').value;
 
-function optionSelected(){
-const leaveType = document.querySelector('#leave_type').value
-
-if (leaveType == "Casual") {
-    console.log(leaveType)
-
+  if (leaveType == 'Casual') {
+    console.log(leaveType);
+  } else if (leaveType == 'Sick') {
+    console.log(leaveType);
+  } else if (leaveType == 'Floater') {
+    console.log(leaveType);
+  }
 }
-
-else if (leaveType == "Sick") {
-    console.log(leaveType)
-
-}
-else if (leaveType == "Floater") {
-    console.log(leaveType)
-
-}
-
-
-// fetch("/api/leaves/getleaveType",options)
-// .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data)
-//   })
-//   .catch((error) => {
-//     console.log(error);
-    
-//   });
-
-}
-
